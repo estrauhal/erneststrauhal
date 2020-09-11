79980b95d51ebee0ce83ce7ce023199573138d28
Ernest Strauhal <estrauhal@gmail.com>
2020-09-10 21:18:47 -0700

    jklafdsklsdfa

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-	function myFunction() {
-		document.getElementById("contact").style.display = "none";
+function myFunction() {
+	document.getElementById("contact").style.display = "none";
+}
+
+function myFunction() {
+	var x = document.getElementById("contact");
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+dragElement(document.getElementById("contact"));
+
+function dragElement(elmnt) {
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	if (document.getElementById(elmnt.id + "header")) {
+		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+	} else {
+		elmnt.onmousedown = dragMouseDown;
 	}
 
-	function myFunction() {
-		var x = document.getElementById("contact");
-		if (x.style.display == "none") {
-			x.style.display = "block";
-		} else {
-			x.style.display = "none";
-		}
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
 	}
 
-	dragElement(document.getElementById("contact"));
-
-	function dragElement(elmnt) {
-		var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-		if (document.getElementById(elmnt.id + "header")) {
-			document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-		} else {
-			elmnt.onmousedown = dragMouseDown;
-		}
-
-		function dragMouseDown(e) {
-			e = e || window.event;
-			e.preventDefault();
-			pos3 = e.clientX;
-			pos4 = e.clientY;
-			document.onmouseup = closeDragElement;
-			document.onmousemove = elementDrag;
-		}
-
-		function elementDrag(e) {
-			e = e || window.event;
-			e.preventDefault();
-			pos1 = pos3 - e.clientX;
-			pos2 = pos4 - e.clientY;
-			pos3 = e.clientX;
-			pos4 = e.clientY;
-			elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-			elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-		}
-
-		function closeDragElement() {
-			document.onmouseup = null;
-			document.onmousemove = null;
-		}
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
 	}
+
+	function closeDragElement() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
